1dc13a19f48a8d3a751ee6e2e616671131c9cc4f
acexiej-ops <acexiej@gmail.com>
2026-02-22 02:59:03 -0500

    Refactor monitor.py for improved listing checks

--- monitor.py
+++ monitor.py
@@ -1,66 +1,53 @@
+import os
 import requests
 import json
-import os
 from bs4 import BeautifulSoup
 
+WEBHOOK_URL = os.environ.get('DISCORD_WEBHOOK_URL')
 KEYWORDS = ['ryzen', 'intel', 'cpu', 'gpu', 'monitor', 'motherboard', 'laptop']
-FILE_NAME = 'seen_listings.json'
-DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
+SEEN_FILE = 'seen_listings.json'
 
-def send_discord_message(message):
-    if DISCORD_WEBHOOK_URL:
-        try:
-            requests.post(DISCORD_WEBHOOK_URL, json={'content': message})
-        except Exception as e:
-            print(f'Error sending to Discord: {e}')
-    else:
-        print('Discord Webhook URL not set.')
+def load_seen():
+    if os.path.exists(SEEN_FILE):
+        with open(SEEN_FILE, 'r') as f:
+            return set(json.load(f))
+    return set()
 
-def get_listings(keyword):
-    url = f'https://www.trademe.co.nz/a/marketplace/computers/search?search_string={keyword}&sort_order=expirydesc'
+def save_seen(seen):
+    with open(SEEN_FILE, 'w') as f:
+        json.dump(list(seen), f)
+
+def check_keyword(keyword):
+    url = 'https://www.trademe.co.nz/browse/searchresults.aspx?searchstring=' + keyword + '&sort_order=newest'
     headers = {'User-Agent': 'Mozilla/5.0'}
-    try:
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        listings = []
-        for card in soup.find_all('tg-search-card-renderer'):
-            link_tag = card.find('a', href=True)
-            title_tag = card.find('div', class_='tm-marketplace-search-card__title')
-            if link_tag and title_tag:
-                href = link_tag['href']
-                listing_id = href.split('/')[-1].split('?')[0]
-                listings.append({
-                    'id': listing_id,
-                    'title': title_tag.get_text(strip=True),
-                    'url': 'https://www.trademe.co.nz' + href
-                })
-        return listings
-    except Exception as e:
-        print(f'Error fetching listings for {keyword}: {e}')
-        return []
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    listings = []
+    for item in soup.select('.tm-marketplace-search-card__detail-section'):
+        link_el = item.select_one('a')
+        if not link_el: continue
+        title = link_el.get_text(strip=True)
+        url_suffix = link_el.get('href', '')
+        full_url = 'https://www.trademe.co.nz' + url_suffix
+        listing_id = url_suffix.split('/')[-1].split('?')[0]
+        listings.append({'id': listing_id, 'title': title, 'url': full_url})
+    return listings
 
 def main():
-    if os.path.exists(FILE_NAME):
-        with open(FILE_NAME, 'r') as f:
-            seen_ids = set(json.load(f))
-    else:
-        seen_ids = set()
-
-    new_listings_found = False
-    for keyword in KEYWORDS:
-        print(f'Checking keyword: {keyword}')
-        listings = get_listings(keyword)
-        for listing in listings:
-            if listing['id'] not in seen_ids:
-                message = 'New listing found for **' + keyword + '**: ' + listing['title'] + '
-' + listing['url']
-                send_discord_message(message)
-                seen_ids.add(listing['id'])
-                new_listings_found = True
-
-    if new_listings_found:
-        with open(FILE_NAME, 'w') as f:
-            json.dump(list(seen_ids), f)
+    seen = load_seen()
+    new_found = False
+    for kw in KEYWORDS:
+        print('Checking: ' + kw)
+        listings = check_keyword(kw)
+        for l in listings:
+            if l['id'] not in seen:
+                print('New: ' + l['title'])
+                msg = 'New listing for ' + kw + ': ' + l['title'] + ' - ' + l['url']
+                requests.post(WEBHOOK_URL, json={'content': msg})
+                seen.add(l['id'])
+                new_found = True
+    if new_found:
+        save_seen(seen)
 
 if __name__ == '__main__':
     main()
